a263faf59f959dbb0d793385323d78bde6a4d29c
Learnedprawn <vineshparalkar@gmail.com>
2025-09-06 12:37:49 +0530

    string slicing to remove last ]

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,10 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
     {
         // let pattern_without_brackets = pattern[1..( pattern.len()-1 )];
         // input_line.chars().any(|c| c.)
-        let mut pattern_iter = pattern.chars();
-        assert_eq!(pattern_iter.next().unwrap(), '[');
-        for character in pattern_iter {
+        let inner = &pattern[1..pattern.len() - 1];
+        // let mut pattern_iter = pattern.chars();
+        // assert_eq!(pattern_iter.next().unwrap(), '[');
+        for character in inner.chars() {
             if input_line.contains(character) {
                 return true;
             }
