d9f6c19336191f47b61a538ae9e345593ddca84f
Learnedprawn <vineshparalkar@gmail.com>
2025-09-06 13:15:40 +0530

    added negative char groups which should work

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,9 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
             == '^'
         {
             let inner = pattern.trim_start_matches("[^").trim_end_matches(']');
+            input_line.chars().any(|c| !inner.contains(c));
+            // inner.chars().all(|c| )
+
             return true;
         } else {
             let inner = pattern.trim_start_matches('[').trim_end_matches(']');
